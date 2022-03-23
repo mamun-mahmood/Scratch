dbb45ef031994ff7db83f4eb54a6e40654f10090
mamamun009 <mamamun1999@gmail.com>
2022-03-23 17:58:56 +0600

    added filter modal

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -26,7 +26,7 @@ function valuetext(value) {
   
   const minDistance = 1;
 export default function BasicModal() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
